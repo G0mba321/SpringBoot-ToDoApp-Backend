dc8fee51044fa3e1f3066a59cf6f9263d75922ec
arte9 <arte9m@gmail.com>
2025-11-24 00:49:15 +0200

    provided @RestControllerAdvice and improve exceptions

---
+++ src/main/java/com/todo/TodoApp/exceptions/AllExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.todo.TodoApp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AllExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
+}
